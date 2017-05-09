dd51c00581e65399cb519b1341d7cd7a4dbf673d
Andres Saldana <pollyketpo@gmail.com>
2017-05-09 03:41:51 -0500

    Calculadora de distribucion normal 95% completa
    precision en calculos y mejorar interfaz

---
+++ DistribucionBinomial/distribucionBinomial/src/distBin/Logica.java
@@ -0,0 +1,44 @@
+package distBin;
+
+/**
+ * @author ANDRES
+ */
+public class Logica {
+
+    double p, n, x;
+
+    public Logica(double n, double x, double p) {
+        this.n = n;
+        this.x = x;
+        this.p = p;
+    }
+
+    double factorial(double num) {
+        if (num == 0) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+    
+    double Binomial(double n, double x, double p){
+        double resultado;
+        resultado=(factorial(n)/(factorial(x)*factorial(n-x)))*(Math.pow(p,x))*(Math.pow(1-p,n-x));
+        return resultado;
+    }
+    
+    double Sumatoria(){
+        double aux=0,total=0;
+        do{
+            System.out.println(aux);
+            total=total+Binomial(n,aux,p);
+            aux++;            
+        }while(aux<=x);
+        return total;
+    }
+
+    public static void main(String args[]) {
+        Logica l=new Logica(2,1,0.25);
+        System.out.print(l.Sumatoria());
+    }
+}
